fc5f488a1789cdbf150c258ce8041805313a92a7
Josip Kelecic <kelecicj@gmail.com>
2024-09-07 13:30:49 +0200

    universal9925: initial init files

--- common.mk
+++ common.mk
@@ -60,13 +60,13 @@ PRODUCT_COPY_FILES += \
 
 # init
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/init/fstab.exynos2100:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.exynos2100 \
-    $(COMMON_PATH)/configs/init/fstab.exynos2100:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/fstab.exynos2100 \
-    $(COMMON_PATH)/configs/init/fstab.universal2100:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.exynos2100 \
-    $(COMMON_PATH)/configs/init/init.exynos2100.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos2100.rc \
-    $(COMMON_PATH)/configs/init/init.exynos2100.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos2100.usb.rc \
-    $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.samsung.rc \
-    $(COMMON_PATH)/configs/init/init.recovery.exynos2100.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.exynos2100.rc \
+    $(COMMON_PATH)/configs/init/fstab.s5e9925:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.s5e9925 \
+    $(COMMON_PATH)/configs/init/fstab.s5e9925:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/fstab.s5e9925 \
+    $(COMMON_PATH)/configs/init/fstab.s5e9925:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.s5e9925 \
+    $(COMMON_PATH)/configs/init/init.s5e9925.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.s5e9925.rc \
+    $(COMMON_PATH)/configs/init/init.s5e9925.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.s5e9925.usb.rc \
+    $(COMMON_PATH)/configs/init/init.recovery.s5e9925.rc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/init.recovery.s5e9925.rc \
+    $(COMMON_PATH)/configs/init/init.recovery.s5e9925.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.s5e9925.rc \
     $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
     $(COMMON_PATH)/configs/init/init.udfps.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.udfps.rc
 
